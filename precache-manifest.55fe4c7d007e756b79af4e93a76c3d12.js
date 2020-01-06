15e319199cd46cc42f48673870069b47ab754372
Gal Kol <999galk@gmail.com>
2020-01-06 20:43:18 +0200

    Updates

---
+++ precache-manifest.55fe4c7d007e756b79af4e93a76c3d12.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2f5dfd673e29e945dc9c192fa46c39",
+    "url": "/MoodLifterApp/index.html"
+  },
+  {
+    "revision": "39c21fe9dfdb389bd9d2",
+    "url": "/MoodLifterApp/static/css/2.70550239.chunk.css"
+  },
+  {
+    "revision": "7878242ca8520e8f861a",
+    "url": "/MoodLifterApp/static/css/main.70743994.chunk.css"
+  },
+  {
+    "revision": "39c21fe9dfdb389bd9d2",
+    "url": "/MoodLifterApp/static/js/2.13f91c21.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/MoodLifterApp/static/js/2.13f91c21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7878242ca8520e8f861a",
+    "url": "/MoodLifterApp/static/js/main.e825a73e.chunk.js"
+  },
+  {
+    "revision": "aefb5faf17129e6fcd51",
+    "url": "/MoodLifterApp/static/js/runtime-main.9f6a218a.js"
+  },
+  {
+    "revision": "b7eb65a491ef0a980adb5067afe35758",
+    "url": "/MoodLifterApp/static/media/PoetsenOne-Regular.b7eb65a4.woff"
+  },
+  {
+    "revision": "6bd6880b166bb79c4f6f3b2fe0f5cfc5",
+    "url": "/MoodLifterApp/static/media/Promo-Twitter-Header.6bd6880b.jpeg"
+  },
+  {
+    "revision": "29df7a58fc81c9049865a14e2c2dac79",
+    "url": "/MoodLifterApp/static/media/logoMeme.29df7a58.png"
+  },
+  {
+    "revision": "8e18f30c28bdbc1e1c6e855fa1854720",
+    "url": "/MoodLifterApp/static/media/logoTravel.8e18f30c.png"
+  }
+]);
